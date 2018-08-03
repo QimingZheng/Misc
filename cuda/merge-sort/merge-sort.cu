cb23c1f6200da0e8e60a46fd4887ce597e2413f0
QimingZheng <zqmmqz@sjtu.edu.cn>
2018-08-04 07:59:48 +0800

    4th

---
+++ cuda/merge-sort/merge-sort.cu
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include "helper_cuda.h"
+
+void merge(int *array, int start, int mid, int end)
+{       
+        int left_index, right_index, global_index;
+        int left_len = mid - start + 1;
+        int right_len = end - mid;
+        int left[left_len];
+        int right[right_len];
+
+        // initialize left array
+        for (int i = 0; i < left_len; i++) {
+                left[i] = array[start + i];
+        }
+
+        // initialize right array
+        for (int i = 0; i < right_len; i++) {
+                right[i] = array[mid + 1 + i];
+        }
+
+        // index of left array
+        left_index = 0;
+        // index of right array
+        right_index = 0;
+        // index of merged array
+        global_index = start;
+
+        while (left_index < left_len && right_index < right_len) {
+                if (left[left_index] <= right[right_index]) {
+                        array[global_index++] = left[left_index++];
+                } else {
+                        array[global_index++] = right[right_index++];                        
+                }
+        }
+
+        // copy the rest of left array 
+        while (left_index < left_len) {
+                array[global_index++] = left[left_index++];
+        } 
+
+        // copy the rest of right array
+        while (right_index < right_len) {
+                array[global_index++] = right[right_index++];
+        }
+}
+
+void cpu_merge_sort(int *array, int start, int end)
+{
+        if (start >= end)
+                return;
+        
+        int mid = start + (end - start) / 2;
+        cpu_merge_sort(array, start, mid);
+        cpu_merge_sort(array, mid + 1, end);
+        merge(array, start, mid, end);
+}
+
+__global__ void gpu_merge(int *d_in, int *d_out, int size, int sorted_size) 
+{
+        // global ID
+        int gid = blockIdx.x * blockDim.x + threadIdx.x;
+        // start, end of left subarray
+        int left_start = gid * 2 * sorted_size;
+        int left_end = min((gid * 2 + 1) * sorted_size - 1, size - 1);
+        // start, end of right subarray
+        int right_start = (gid * 2 + 1) * sorted_size;
+        int right_end = min((gid * 2 + 2) * sorted_size - 1, size - 1);
+        
+        int left_index = left_start, right_index= right_start, global_index= left_start;
+
+        while (left_index <= left_end && right_index <= right_end) {
+                if (d_in[left_index] <= d_in[right_index]) {
+                        d_out[global_index++] = d_in[left_index++];
+                } else {
+                        d_out[global_index++] = d_in[right_index++];                        
+                }
+        }
+
+        while (left_index <= left_end) {
+                d_out[global_index++] = d_in[left_index++];                
+        }
+
+        while (right_index <= right_end) {
+                d_out[global_index++] = d_in[right_index++];                  
+        }
+}
+
+void gpu_merge_sort(int *h_in, int size)
+{
+        int *d_in, *d_out, *tmp;
+
+        if (size == 1)
+                return;
+        
+	// allocate GPU memory
+	checkCudaErrors(cudaMalloc((void**)&d_in, size * sizeof(int)));
+        checkCudaErrors(cudaMalloc((void**)&d_out, size * sizeof(int)));
+
+        // copy input from host memory to GPU memory
+        cudaMemcpy(d_in, h_in, size * sizeof(int), cudaMemcpyHostToDevice);
+
+        // # of threads per block
+        int threads_per_block = 1024;
+        int sorted_size = 1;
+        // # of blocks 
+        int blocks = 1;
+
+        while (sorted_size < size) {
+                // each thread can merge at most 2 * sorted_size elements
+                // how many threads do we need in total?
+                int threads_total = (size + 2 * sorted_size - 1) / (2 * sorted_size);
+                // total # of blocks that we need
+                blocks = (threads_total + threads_per_block - 1) / threads_per_block;
+
+                gpu_merge<<<blocks, threads_per_block>>>(d_in, d_out, size, sorted_size);
+                
+                sorted_size *= 2;
+                // exchange input and output
+                tmp = d_in;
+                d_in = d_out;
+                d_out = tmp;
+        }
+
+        // copy output from GPU memory to host memory
+        cudaMemcpy(h_in, d_in, size * sizeof(int), cudaMemcpyDeviceToHost);
+
+        // free GPU memory
+        cudaFree(d_in);
+        cudaFree(d_out);
+}
+
+int main()
+{
+        int array_size = 111111;
+        int array[array_size];
+        // sort result computed by GPU
+        int h_in[array_size];
+        bool result;
+
+        // initialize random number generator
+        srand(time(NULL));
+
+        printf("Input\n");
+        for (int i = 0; i < array_size; i++) {
+                array[i] = rand() % array_size;
+                h_in[i] = array[i];
+                printf("%d ", array[i]);
+        }
+        printf("\n");
+
+        // merge sort on CPU
+        cpu_merge_sort(array, 0, array_size - 1);
+        printf("Expected Output\n");
+        for (int i = 0; i < array_size; i++) {
+                printf("%d ", array[i]);
+        }         
+        printf("\n");        
+        
+        // merge sort on GPU
+        gpu_merge_sort(h_in, array_size);
+        printf("GPU Output\n"); 
+        result = true;
+        for (int i = 0; i < array_size; i++) {
+                printf("%d ", h_in[i]);
+                if (h_in[i] != array[i]) {
+                        result = false;
+                }
+        }        
+        printf("\n");
+
+        if (result) {
+                printf("Correct\n");
+        } else {
+                printf("Wrong\n");
+        }
+        
+        return 0;
+}
---
+++ cuda/reduce/reduce.cu
@@ -0,0 +1,590 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+// Interleaved addressing with divergent branching
+__global__ void reduce_kernel0(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Copy elements from global memoery into per-block shared memory
+    s_data[tid] = d_in[gid];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();
+
+    // s = 1, 2, 4, 8, ..... blockDim.x / 2
+    for (unsigned int s = 1; s < blockDim.x; s <<= 1) {
+        if (tid % (s << 1) == 0) {
+            s_data[tid] += s_data[tid + s];
+        }
+        // Ensure all threads in the block finish add in this round
+        __syncthreads();
+    }
+
+    // write the reduction sum back to the global memory
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }
+}
+
+// Interleaved addressing with bank conflicts
+__global__ void reduce_kernel1(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Copy elements from global memoery into per-block shared memory
+    s_data[tid] = d_in[gid];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();    
+
+    // s = 1, 2, 4, 8, ..... blockDim.x / 2
+    for (unsigned int s = 1; s < blockDim.x; s <<= 1) {
+        int index = tid * s * 2;
+
+        if (index + s < blockDim.x) {
+            s_data[index] += s_data[index + s];
+        }
+
+        // Ensure all threads in the block finish add in this round
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }
+}
+
+// Sequential addressing
+__global__ void reduce_kernel2(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Copy elements from global memoery into per-block shared memory
+    s_data[tid] = d_in[gid];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();    
+
+    // s = blockDim.x / 2, ....., 8, 4, 2, 1
+    for (unsigned int s = (blockDim.x >> 1); s >= 1; s >>= 1) {
+        if (tid < s) {
+            s_data[tid] += s_data[tid + s];
+        }
+        // Ensure all threads in the block finish add in this round
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }
+}
+
+// First add during global load
+__global__ void reduce_kernel3(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+
+    // perform first level of reduction,
+    // reading from global memory, writing to shared memory
+    s_data[tid] = d_in[gid] + d_in[gid + blockDim.x];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();    
+
+    // s = blockDim.x / 2, ....., 8, 4, 2, 1
+    for (unsigned int s = (blockDim.x >> 1); s >= 1; s >>= 1) {
+        if (tid < s) {
+            s_data[tid] += s_data[tid + s];
+        }
+        // Ensure all threads in the block finish add in this round
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }    
+}
+
+__device__ void warpReduce4(volatile int* s_data, int tid) 
+{
+    s_data[tid] += s_data[tid + 32];
+    s_data[tid] += s_data[tid + 16];
+    s_data[tid] += s_data[tid + 8];
+    s_data[tid] += s_data[tid + 4];
+    s_data[tid] += s_data[tid + 2];
+    s_data[tid] += s_data[tid + 1];
+}
+
+// Unroll the last warp
+__global__ void reduce_kernel4(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+
+    // perform first level of reduction,
+    // reading from global memory, writing to shared memory
+    s_data[tid] = d_in[gid] + d_in[gid + blockDim.x];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();    
+
+    // s = blockDim.x / 2, ....., 128, 64
+    for (unsigned int s = (blockDim.x >> 1); s > 32; s >>= 1) {
+        if (tid < s) {
+            s_data[tid] += s_data[tid + s];
+        }
+        // Ensure all threads in the block finish add in this round
+        __syncthreads();
+    }
+
+    // now we only have 32 active threads (a warp) left
+    if (tid < 32) {
+        warpReduce4(s_data, tid);
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }    
+}
+
+template <unsigned int block_size> __device__ void warpReduce5(volatile int* s_data, int tid) 
+{
+    if (block_size >= 64) {
+        s_data[tid] += s_data[tid + 32];
+    }
+
+    if (block_size >= 32) {
+        s_data[tid] += s_data[tid + 16];
+    }
+
+    if (block_size >= 16) {
+        s_data[tid] += s_data[tid + 8];
+    }
+
+    if (block_size >= 8) {
+        s_data[tid] += s_data[tid + 4];
+    }
+
+    if (block_size >= 4) {
+        s_data[tid] += s_data[tid + 2];
+    }
+
+    if (block_size >= 2) {
+        s_data[tid] += s_data[tid + 1];
+    }
+}
+
+// Completely unrolled
+// The block size is limited to 1024 threads.
+// Also, we are sticking to power-of-2 block sizesSo
+template <unsigned int block_size> __global__ void reduce_kernel5(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+
+    // perform first level of reduction,
+    // reading from global memory, writing to shared memory
+    s_data[tid] = d_in[gid] + d_in[gid + blockDim.x];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();
+
+    if (block_size >= 1024) {
+        if (tid < 512) { 
+            s_data[tid] += s_data[tid + 512]; 
+        }
+        __syncthreads(); 
+    } 
+    
+    if (block_size >= 512) {
+        if (tid < 256) { 
+            s_data[tid] += s_data[tid + 256]; 
+        }
+        __syncthreads(); 
+    } 
+
+    if (block_size >= 256) {
+        if (tid < 128) { 
+            s_data[tid] += s_data[tid + 128]; 
+        }
+        __syncthreads(); 
+    } 
+
+    if (block_size >= 128) {
+        if (tid < 64) { 
+            s_data[tid] += s_data[tid + 64]; 
+        }
+        __syncthreads(); 
+    }
+
+    // only a warp left
+    if (tid < 32) {
+        warpReduce5<block_size>(s_data, tid); 
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }   
+}
+
+// wrapper function to run reduce_kernel5
+void run_reduce_kernel5(int *d_out, int *d_in, unsigned int blocks, unsigned int threads)
+{
+    //printf("threads = %u\n", threads);
+    switch (threads) {
+        case 1024:
+            reduce_kernel5<1024><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);
+            break;
+        case 512:
+            reduce_kernel5< 512><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);
+            break;
+        case 256:
+            reduce_kernel5< 256><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        case 128:
+            reduce_kernel5< 128><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        case 64:
+            reduce_kernel5<  64><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        case 32:
+            reduce_kernel5<  32><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);
+            break;
+        case 16:
+            reduce_kernel5<  16><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        case 8:
+            reduce_kernel5<   8><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        case 4:
+            reduce_kernel5<   4><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        case 2:
+            reduce_kernel5<   2><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        case 1:
+            reduce_kernel5<   1><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in);        
+            break;
+        default:
+            printf("Invalid block size %u\n", threads);                        
+    }
+}
+
+// Add multiple elements per thread
+template <unsigned int block_size> __global__ void reduce_kernel6(int *d_out, int *d_in, unsigned int size)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+    // total # of threads 
+    unsigned int stride = gridDim.x * blockDim.x;
+
+    // perform sequential reduction
+    s_data[tid] = 0;
+    while (gid < size) {
+        s_data[tid] += d_in[gid];
+        gid += stride;
+    }
+    __syncthreads();
+
+    if (block_size >= 1024) {
+        if (tid < 512) { 
+            s_data[tid] += s_data[tid + 512]; 
+        }
+        __syncthreads(); 
+    } 
+    
+    if (block_size >= 512) {
+        if (tid < 256) { 
+            s_data[tid] += s_data[tid + 256]; 
+        }
+        __syncthreads(); 
+    } 
+
+    if (block_size >= 256) {
+        if (tid < 128) { 
+            s_data[tid] += s_data[tid + 128]; 
+        }
+        __syncthreads(); 
+    } 
+
+    if (block_size >= 128) {
+        if (tid < 64) { 
+            s_data[tid] += s_data[tid + 64]; 
+        }
+        __syncthreads(); 
+    }
+
+    // only a warp left
+    if (tid < 32) {
+        warpReduce5<block_size>(s_data, tid); 
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }   
+}
+
+// wrapper function to run reduce_kernel6
+void run_reduce_kernel6(int *d_out, int *d_in, unsigned int size, unsigned int blocks, unsigned int threads)
+{
+    switch (threads) {
+        case 1024:
+            reduce_kernel6<1024><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);
+            break;
+        case 512:
+            reduce_kernel6< 512><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);
+            break;
+        case 256:
+            reduce_kernel6< 256><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        case 128:
+            reduce_kernel6< 128><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        case 64:
+            reduce_kernel6<  64><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        case 32:
+            reduce_kernel6<  32><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);
+            break;
+        case 16:
+            reduce_kernel6<  16><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        case 8:
+            reduce_kernel6<   8><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        case 4:
+            reduce_kernel6<   4><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        case 2:
+            reduce_kernel6<   2><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        case 1:
+            reduce_kernel6<   1><<<blocks, threads, threads * sizeof(int)>>>(d_out, d_in, size);        
+            break;
+        default:
+            printf("Invalid block size %u\n", threads);                        
+    }
+}
+
+inline bool is_power_of_2(int n)
+{
+    return ((n & (n - 1)) == 0);
+}
+
+void print_kernel_info(int kernel_id) 
+{
+    switch (kernel_id) {
+        case 0: 
+            printf("Interleaved addressing with divergent branching\n");
+            break;
+        case 1:
+            printf("Interleaved addressing with bank conflicts\n");
+            break;  
+        case 2:
+            printf("Sequential addressing\n");
+            break;
+        case 3:
+            printf("First add during global load\n");
+            break;
+        case 4:
+            printf("Unroll last warp\n");
+            break;
+        case 5:
+            printf("Completely unrolled\n");
+            break;
+        case 6:
+            printf("Multiple elements per thread\n");
+            break;              
+        default:
+            printf("Invalid kernel function ID %d\n", kernel_id);        
+    }    
+}
+
+// input: array (in host memory), array size, expected result, kernel function ID and iterations 
+void reduce(int *h_in, int array_size, int expected_result, int kernel_id, int iters)
+{
+    // # of threads per block. It should be the power of two
+    int threads = 1 << 10;
+    // # of blocks in total. 
+    int blocks = 1;
+    // GPU memory pointers
+    int *d_in, *d_intermediate, *d_out;
+    // final result in host memory
+    int h_out;
+    // events to record start and stop time
+    cudaEvent_t start, stop;
+    // elapsed time
+    float elapsed_time;
+
+    // print kernel information
+    print_kernel_info(kernel_id); 
+    
+    if (!h_in || array_size <= 0 || !is_power_of_2(array_size)) {
+        goto out;
+    }
+
+    if (array_size > threads) {
+        blocks = array_size / threads;
+    }
+
+    // If each thread can reduce multiple elements
+    if (kernel_id == 6) {
+        blocks = 256;
+        threads = 256;
+    }
+
+    // allocate GPU memory
+    if (cudaMalloc((void**) &d_in, array_size * sizeof(int)) != cudaSuccess
+     || cudaMalloc((void**) &d_intermediate, blocks * sizeof(int)) != cudaSuccess
+     || cudaMalloc((void**) &d_out, sizeof(int)) != cudaSuccess)
+        goto out;
+
+    // copy the input array from the host memory to the GPU memory
+    cudaMemcpy(d_in, h_in, array_size * sizeof(int), cudaMemcpyHostToDevice);
+
+    // create events
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start, 0);
+
+    // run many times
+    for (int i = 0; i < iters; i++) {
+        switch (kernel_id) {
+            // Interleaved addressing with divergent branching 
+            case 0: 
+                reduce_kernel0<<<blocks, threads, threads * sizeof(int)>>>(d_intermediate, d_in); 
+                reduce_kernel0<<<1, blocks, blocks * sizeof(int)>>>(d_out, d_intermediate);
+                break;
+            // Interleaved addressing with bank conflicts
+            case 1:
+                reduce_kernel1<<<blocks, threads, threads * sizeof(int)>>>(d_intermediate, d_in);
+                reduce_kernel1<<<1, blocks, blocks * sizeof(int)>>>(d_out, d_intermediate);
+                break;  
+            // Sequential addressing              
+            case 2:
+                reduce_kernel2<<<blocks, threads, threads * sizeof(int)>>>(d_intermediate, d_in);
+                reduce_kernel2<<<1, blocks, blocks * sizeof(int)>>>(d_out, d_intermediate);
+                break;
+            // First add during global load
+            case 3:
+                reduce_kernel3<<<blocks, threads / 2 , threads / 2 * sizeof(int)>>>(d_intermediate, d_in);
+                reduce_kernel3<<<1, blocks / 2, blocks / 2 * sizeof(int)>>>(d_out, d_intermediate);  
+                break;
+            // Unroll last warp
+            case 4:
+                reduce_kernel4<<<blocks, threads / 2 , threads / 2 * sizeof(int)>>>(d_intermediate, d_in);
+                reduce_kernel4<<<1, blocks / 2, blocks / 2 * sizeof(int)>>>(d_out, d_intermediate);
+                break;
+            // Completely unrolled
+            case 5:
+                run_reduce_kernel5(d_intermediate, d_in, blocks, threads / 2);
+                run_reduce_kernel5(d_out, d_intermediate, 1, blocks / 2);               
+                break;
+            // Multiple elements per thread
+            case 6:
+                run_reduce_kernel6(d_intermediate, d_in, array_size, blocks, threads);
+                run_reduce_kernel6(d_out, d_intermediate, blocks, 1, blocks / 2);             
+                break;                          
+            default:
+                printf("Invalid kernel function ID %d\n", kernel_id);   
+                goto out;      
+        }
+    }
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&elapsed_time, start, stop);    
+    elapsed_time /= iters;      
+    printf("Average time elapsed: %f ms\n", elapsed_time);
+
+    // destroy events
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    // copy the result from the GPU memory to the host memory
+    cudaMemcpy(&h_out, d_out, sizeof(int), cudaMemcpyDeviceToHost);
+
+    if (h_out != expected_result) {
+        printf("Wrong result: %d (expected) %d (actual)\n", expected_result, h_out);
+    }
+
+out:
+    // free GPU memory
+    cudaFree(d_in);
+    cudaFree(d_intermediate);
+    cudaFree(d_out);
+}
+
+// generate a random integer in [min, max]
+inline int random_range(int min, int max)
+{
+    if (min > max)
+        return 0;
+    else
+        return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
+
+int main(int argc, char **argv) 
+{
+    if (argc != 3) {
+        printf("%s [kernel ID] [iterations]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int kernel_id = atoi(argv[1]);
+    int iters = atoi(argv[2]);
+    if (iters <= 0 || kernel_id < 0) {
+        printf("Invalid input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    const int ARRAY_SIZE = 1 << 20;
+    int h_in[ARRAY_SIZE];
+    int sum = 0;
+    
+    // initialize random number generator
+    srand(time(NULL));
+    int min = 0, max = 10;
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        // generate a random int in a range
+        h_in[i] = random_range(min, max);
+        sum += h_in[i];
+    }
+
+    // launch reduce kernels
+    reduce(h_in, ARRAY_SIZE, sum, kernel_id, iters);
+
+    return 0;
+}
